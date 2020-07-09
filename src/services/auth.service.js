6238b64306e1096ae39ea13711234f0ee431c098
박종민 <jmpark93@koscom.co.kr>
2020-07-09 18:56:12 +0900

    update ...

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
+import authHeader from './auth-header';
 
 // const API_URL = 'http://localhost:8081/api/auth/';
 const API_URL = process.env.VUE_APP_AUTHAPI;
@@ -6,40 +7,41 @@ const API_URL = process.env.VUE_APP_AUTHAPI;
 class AuthService {
   login(user) {
     return axios
-      .post(API_URL + 'signin', {
+      .post(API_URL + "signin", {
         username: user.username,
-        password: user.password
+        password: user.password,
       })
-      .then(response => {
+      .then((response) => {
         if (response.data.accessToken) {
-          localStorage.setItem('user', JSON.stringify(response.data));
-        } 
-        
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
         return response.data;
       });
   }
 
   logout() {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
   }
 
   register(user, imageFile) {
-    const formData = new FormData(); 
+    const formData = new FormData();
 
-    formData.append('profile', JSON.stringify(user))
+    formData.append("profile", JSON.stringify(user));
 
-    if( imageFile ) {
-      formData.append('imageFile', imageFile);
+    if (imageFile) {
+      formData.append("imageFile", imageFile);
     }
 
-    return axios.post(API_URL + 'signup', 
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data"
-        }
-      }
-    );
+    return axios.post(API_URL + "signup", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }
+
+  delete(userId) {
+    return axios.delete(API_URL + userId, { headers: authHeader() });
   }
 }
 
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -11,66 +11,124 @@
         hide-details
       ></v-text-field>
     </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="users"
-      :search="search"
-    >
-      <template v-slot:item.roles="{ item }"> 
+
+    <v-data-table :headers="headers" :items="users" :search="search">
+      <template v-slot:item.roles="{ item }">
         <template v-for="role in item.roles"> {{ role.name }}, </template>
       </template>
+
+      <template v-slot:item.actions="{ item }">
+        <v-icon small @click="openDialog(item)">
+          mdi-delete
+        </v-icon>
+      </template>
     </v-data-table>
+
+    <v-dialog v-model="dialog" max-width="290" >
+      <v-card>
+        <v-card-title class="headline">사용자 삭제 ... </v-card-title>
+
+        <v-card-text v-if="users[deleteIdx]">
+          사용자 "{{
+            users[deleteIdx].username + "(" + users[deleteIdx].id + ")"
+          }}"를 삭제할까요 ?
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="green darken-1" text @click="dialog = false">
+            NO
+          </v-btn>
+
+          <v-btn color="green darken-1" text @click="deleteUser">
+            YES
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
-import UserService from '../services/user.service';
+import UserService from "../services/user.service";
+import AuthService from "../services/auth.service";
 
 export default {
   name: "Admin",
-  
+
   data: () => ({
-    search: '',
+    search: "",
     headers: [
       {
-        text: 'ID',
-        align: 'start',
+        text: "ID",
+        align: "start",
         sortable: false,
-        value: 'id',
+        value: "id",
       },
-      { text: 'User Name', value: 'username' },
-      { text: 'E-Mail', value: 'email' },
-      { text: 'Role', value: 'roles' }
+      { text: "User Name", value: "username" },
+      { text: "E-Mail", value: "email" },
+      { text: "Role", value: "roles" },
+      { text: "Actions", value: "actions", sortable: false },
     ],
-    users: []
+    users: [],
+    dialog: false,
+    deleteIdx: 0,
   }),
 
   computed: {
     currentUser() {
       return this.$store.state.auth.user;
-    }
+    },
   },
 
   mounted() {
     if (!this.currentUser) {
-      this.$router.push('/login');
+      this.$router.push("/login");
     }
 
-    console.log( "User ID : " + this.currentUser.id);
+    console.log("User ID : " + this.currentUser.id);
 
     UserService.getAllUsers().then(
-      response => {
+      (response) => {
         this.users = response.data;
       },
-      error => {
+      (error) => {
         this.content =
-          (error.response && error.response.data) &&
-          error.message ||
+          (error.response && error.response.data && error.message) ||
           error.toString();
 
-        console.log( this.content);
+        console.log(this.content);
       }
     );
-  }
-}
+  },
+
+  methods: {
+    openDialog(item) {
+      this.deleteIdx = this.users.indexOf(item);
+      this.dialog = true;
+    },
+
+    deleteUser() {
+      this.dialog = false;
+      console.log("Delete User : " + this.users[this.deleteIdx].username + "(" + this.users[this.deleteIdx].id + ")");
+
+      AuthService.delete( this.users[this.deleteIdx].id ).then(
+        (response) => {
+          console.log("Delete User : Success");
+          console.log(response);
+          this.users.splice(this.deleteIdx,1);
+        },
+        (error) => {
+          this.content =
+            (error.response && error.response.data && error.message) ||
+            error.toString();
+
+          console.log(this.content);
+          console.log("Delete User : Failed");
+        }
+      );
+    },
+  },
+};
 </script>
